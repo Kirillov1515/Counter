42992341f4b26f549d47103597e195f611ed687e
Kirillov Alexander <kirillov_sasha98@mail.ru>
2023-06-15 12:51:51 +0300

    Реализована усложненная задача 3 спринта - Счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counter: Int = 0
+    
+    @IBOutlet weak var counterValueLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var databaseOfChangesTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setToCounterValueLabel(value: counter)
+        
+        plusButton.tintColor = .red
+        minusButton.tintColor = .blue
+        zeroButton.tintColor = .green
+        databaseOfChangesTextView.text = "История изменений:"
     }
-
-
+    
+    
+    @IBAction func plusButtonPressed() {
+        counter += 1
+        setToCounterValueLabel(value: counter)
+        databaseOfChangesTextView.text += "\n\(getData()): значение изменено на +1"
+    }
+    
+    @IBAction func minusButtonPressed() {
+        if counter == 0 {
+            databaseOfChangesTextView.text += "\n\(getData()): попытка уменьшить значение счётчика ниже 0"
+        } else {
+            counter -= 1
+            setToCounterValueLabel(value: counter)
+            databaseOfChangesTextView.text += "\n\(getData()): значение изменено на -1"
+        }
+    }
+    
+    @IBAction func zeroButtonPressed() {
+        counter = 0
+        setToCounterValueLabel(value: counter)
+        databaseOfChangesTextView.text += "\n\(getData()): значение сброшено"
+    }
+    
+    func setToCounterValueLabel(value: Int) {
+        counterValueLabel.text = "Значение счётчика: \(value)"
+    }
+    
+    func getData() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.YY HH:mm:ss"
+        return dateFormatter.string(from: Date())
+    }
 }
 
